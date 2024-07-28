944d289776a9574b04203a37e01d859558b57128
Geetika <pappygarg77@gmail.com>
2024-07-28 15:42:03 +0530

    index.js

--- api/index.js
+++ api/index.js
@@ -15,9 +15,6 @@ app.use(express.json());
 app.use(cookieParser());
 
 
-
-const mongoURI = process.env.MONGO_URI ;
-
 mongoose.connect(process.env.MONGO_URI, {
     // useNewUrlParser: true,
     // useUnifiedTopology: true,
