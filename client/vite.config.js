76b94fb2b29a731748afcd8dd34e9f2e39c52073
Geetika <pappygarg77@gmail.com>
2024-08-02 20:48:58 +0530

    changed target

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://estate-service.onrender.com',
         changeOrigin: true,
         secure: false,
         timeout: 5000,
